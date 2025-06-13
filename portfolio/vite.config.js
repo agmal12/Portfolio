7e08867acf73a35c61c814678364846df48c517c
agmal12 <u2204005@rajagiri.edu.in>
2025-06-13 12:56:06 +0530

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE_BASE_PATH || "/Portfolio",
+  
   optimizeDeps: {
     include: ['@react-three/fiber', '@react-three/drei', 'three'],
     exclude: ['troika-three-text'] // Sometimes helps with drei
